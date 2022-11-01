40e95becc0b3e6eb219fc1e790cfc54b956cc278
NguyenHien <nguyenhien2942000@gmail.com>
2022-11-01 14:27:54 +0700

    push homepage 01/11/2022

---
+++ utils/validemail/index.js
@@ -0,0 +1,5 @@
+const validEmail = (email) => {
+    const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexEmail.test(email);
+  };
+  export default validEmail;
