bb18ab34df4ffe839a3579aafd60bf02ef07e91c
NguyenHien <nguyenhien2942000@gmail.com>
2022-11-10 15:23:39 +0700

    add header components

--- pages/home.js
+++ pages/home.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import requests from 'utils/API';
+import Header from 'components/Header';
 
 const home = ({ popularMovies }) => {
   const movies = popularMovies.results;
-  console.log(movies);
+
   return (
     <div>
-      {movies.map((item) => {
-        return (
-          <>
-            <h1>{item.title}</h1>
-            <img 
-            src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`} 
-            width="100"
-            alt={item.title}></img>
-            
-          </>
-        );
-      })}
+      <Header/>
       <button>
         <a href="/">home page</a>
       </button>
